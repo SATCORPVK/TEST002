e6bfcf8306ffe952e3734d78ca0de4ebfdf0cf0b
ANU <Satcorpvk@outlook.com>
2026-02-08 22:55:27 -0800

    Update vanta.bg.js

--- js/vanta.bg.js
+++ js/vanta.bg.js
@@ -1,1 +1,15 @@
-
+// Vanta.js Interactive Background
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x9b4dff,
+  backgroundColor: 0x111111,
+  points: 12.00,
+  maxDistance: 20.00,
+  spacing: 15.00
+});
